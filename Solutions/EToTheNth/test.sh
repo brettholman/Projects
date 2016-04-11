0742f273e721bd59132cc6440923672ad143a0af
Brett Holman <brettholman91@gmail.com>
2016-04-11 16:29:39 -0700

    Added work for E to the Nth, including make file and test suite for bash

---
+++ Solutions/EToTheNth/test.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+# Recompiles the file "Pi.cpp" and runs test suite.
+	
+make clean > /dev/null 2>&1
+
+make > /dev/null
+
+# Test input for numbers 
+for i in {-10..47}
+do
+	echo "./e " $i
+	./e $i
+	echo
+done
+
+# Test inputs for other values
+
+# Works, considered to be 19
+echo "./e " 19ff
+./e 19ff
+echo
+
+# Does not work
+echo "./e " ff19
+./e ff19
+echo
+
+echo End of Tests
+
+

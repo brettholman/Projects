7cd481a93c1d0d38cc6d967c29c5e40fd0e4f4dd
Brett Holman <brettholman91@gmail.com>
2016-05-14 17:50:42 -0700

    Updated files to remove excess function.

--- Solutions/Scala/PiToTheNth/Pi.scala
+++ Solutions/Scala/PiToTheNth/Pi.scala
@@ -3,8 +3,6 @@ import java.text.DecimalFormat
 object Pi {
   private var formatter = new DecimalFormat("#.###")
 
-  private def isAllDigits(x: String) = x forall Character.isDigit
-
   def main(args: Array[String]): Unit = {
     if(args.length != 1) {
       println("There must be exactly one argument passed to this program")
@@ -12,7 +10,7 @@ object Pi {
     }
 
     if(args(0)(0) != '-') {
-      if(!isAllDigits(args(0))) {
+      if(!(args(0) forall Character.isDigit)) {
         println("You can not pass a non numeric value into this program.")
         println(args(0) + " Is not a valid input. ")
         return
